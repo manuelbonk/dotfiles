cb88894d3971f01026514f9550f065a2b934926a
Manuel Bonk <manuel@bonkii.com>
2021-05-27 13:34:30 +0200

    add fish stuff and some minor adjustments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,8 +13,6 @@ alias lf='ls -xhFl --color=auto'
 alias ls='ls --color=auto'
 alias lc='wc -l'
 alias cx='chmod +x'
-alias pdf='okular'
-alias free='free -ms 1'
 alias grep='grep -i --color=auto'
 
 alias pacupg='sudo pacman -Syu'
@@ -25,23 +23,18 @@ alias pacrem='sudo pacman -Rnsc'        # Remove the specified package(s), its c
 alias pacrep='pacman -Si'              # Display information about a given package in the repositories
 alias pacreps='pacman -Ss'
 
-alias rmvunused='sudo pacman -Rns $(pacman -Qqtd)' # remove unused packages
-alias cf='echo $(($(ls -l | grep -v ^d| wc -l)-1))' # count files in a directory
+alias rmvunused='sudo pacman -Rns (pacman -Qqtd)' # remove unused packages
 alias tsb='sudo tlp-stat -b'
 
 alias ta='tmux attach -t'
 alias tn='tmux new -s'
 alias tls='tmux list-sessions'
 
-alias serial='sudo screen /dev/ttyUSB0 115200'
-alias sudo='sudo '
 alias gig='find ./ -executable ! -path "./.git/*">>.gitignore'
-alias dup='(alacritty --working-directory $(pwd)  2>&1 >/dev/null)&'
 alias ip='ip -c'
 alias mount='sudo mount'
 alias umount='sudo umount'
 alias wifi='sudo wifi'
-alias zshedit='vim ~/.zshrc'
 alias gk='gitk --all'
 alias v='vagrant'
 alias g='git'
@@ -49,14 +42,43 @@ alias ap='ansible-playbook'
 alias a='ansible'
 alias d='docker'
 alias k='kubectl'
-alias c='cloudctl'
-alias kga='kubectl get all'
-alias kx='kubectx'
 alias m='molecule'
 alias sprunge="curl -s -F 'sprunge=<-' http://sprunge.us | xclip -sel c"
 alias cat='cat -v'
-alias flux='xflux -l 11.576124'
-alias p='pass'
-alias tree='tree -C'
-alias kon='kubeon'
-alias kof='kubeoff'
+alias kon='kube_ps on'
+alias koff='kube_ps off'
+alias kx='kubectx'
+alias kns='kubens'
+alias lf='ls -xhFl --color=auto'
+
+# disable vi mode indicator in ps1
+function fish_mode_prompt
+end
+
+# hide kubecontext in ps1 by default
+koff
+
+set -gx PAGER less
+set -gx EDITOR vim
+set -gx BROWSER firefox
+set -gx PATH $PATH $HOME/.screenlayout $HOME/scripts
+set fish_greeting ""
+
+
+function fish_prompt
+    set_color normal
+    # https://stackoverflow.com/questions/24581793/ps1-prompt-in-fish-friendly-interactive-shell-show-git-branch
+    set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s//on /p')
+    set_color blue
+    echo -n (__kube_prompt) ""
+    set_color red
+    echo -n (whoami)'@'(hostname)':'
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+    echo -n ' '
+    set_color purple
+    echo -n "$git_branch"
+    set_color normal
+    echo -e ' \n$ '
+end
---
+++ .config/fish/custom_variables.fish
@@ -0,0 +1,4 @@
+set -gx PAGER less
+set -gx EDITOR vim
+set -gx BROWSER firefox
+set -gx PATH $PATH $HOME/.screenlayout $HOME/scripts
